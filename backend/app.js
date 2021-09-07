1826b7e0c87769645d0806f757a770c517647e50
jdwilkin4 <jwilkin4@hotmail.com>
2021-09-07 02:07:40 -0700

    adding controllers folder

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,16 @@ const port = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const router = require('./controllers/user');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // need to setup mongo uri and check for connection
 
-app.get('/', (req, res) => {
-    res.send("This is the homepage!!!!!!!")
-});
+app.use('/', router);
+app.use(cookieParser());
+app.use(cors());
+
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
